5d935839f1dcaf376b14ca0a48faba7a1265814d
Marko Štuban <marko.stuban@ag04.com>
2018-05-06 14:48:05 +0200

    Initial commit

---
+++ src/test/java/hr/tvz/java/teambuildingbooking/TeamBuildingBookingApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.tvz.java.teambuildingbooking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamBuildingBookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
