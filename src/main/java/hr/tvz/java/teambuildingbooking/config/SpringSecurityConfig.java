8971059132c0db38930d92cc28ea51fdf27c6c76
Marko Štuban <marko.stuban@ag04.com>
2018-05-07 22:35:54 +0200

    podesen bootstrap i jQuery te folder za resurse

--- src/main/java/hr/tvz/java/teambuildingbooking/config/SpringSecurityConfig.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/config/SpringSecurityConfig.java
@@ -1,6 +1,8 @@
 package hr.tvz.java.teambuildingbooking.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalAuthentication
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
