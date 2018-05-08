efb4807ee883565cbccb9d47319a0a35a70a45d1
Marko Štuban <marko.stuban@ag04.com>
2018-05-08 09:15:38 +0200

    Optimized build by removing default context test, added /build to gitignore

--- src/test/java/hr/tvz/java/teambuildingbooking/TeamBuildingBookingApplicationTests.java
+++ src/test/java/hr/tvz/java/teambuildingbooking/TeamBuildingBookingApplicationTests.java
@@ -1,16 +1,8 @@
 package hr.tvz.java.teambuildingbooking;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+/*
+@SpringBootApplication
+ */
 public class TeamBuildingBookingApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
