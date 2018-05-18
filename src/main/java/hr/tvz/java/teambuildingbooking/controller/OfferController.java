ad205cabc6d66c1acfeae4ed1d4e9e9f5dfb6dda
Ana Dedić <anaaadedic@gmail.com>
2018-05-18 15:44:39 +0200

    layout changes, add offer service and repo

--- src/main/java/hr/tvz/java/teambuildingbooking/controller/OfferController.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/controller/OfferController.java
@@ -1,10 +1,17 @@
 package hr.tvz.java.teambuildingbooking.controller;
 
+import hr.tvz.java.teambuildingbooking.model.Offer;
+import hr.tvz.java.teambuildingbooking.service.OfferService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/offer")
 public class OfferController {
@@ -12,6 +19,11 @@ public class OfferController {
     private static final String NEW_OFFER_VIEW_NAME = "offer/new-offer";
     private static final String SEARCH_OFFER_VIEW_NAME = "offer/search-offer";
     private static final String SEARCH_RESULTS_VIEW_NAME = "offer/search-results";
+
+    @Autowired
+    private OfferService offerService;
+
+
 
     @Secured("PROVIDER")
     @RequestMapping("/new")
@@ -25,7 +37,5 @@ public class OfferController {
     }
 
     @RequestMapping("/results")
-    private String showResults(Model model) {
-        return SEARCH_RESULTS_VIEW_NAME;
-    }
+    private String showResults(Model model) { return SEARCH_RESULTS_VIEW_NAME; }
 }
