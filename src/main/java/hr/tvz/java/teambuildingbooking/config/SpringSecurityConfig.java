34d79ac8d85c2306208402ebfed7c5504a953b43
Marko Štuban <marko.stuban@ag04.com>
2018-05-18 11:29:27 +0200

    Added profile paths to SecurityConfig

--- src/main/java/hr/tvz/java/teambuildingbooking/config/SpringSecurityConfig.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/config/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/offer/**")
+                .antMatchers("/", "/offer/**", "/profile/**")
                 .fullyAuthenticated()
                 .and()
                 .formLogin()
