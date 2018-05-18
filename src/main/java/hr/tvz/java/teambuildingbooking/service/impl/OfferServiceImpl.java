50311a1560ccf873657da1453cc1ea667b64af4b
Ana Dedić <anaaadedic@gmail.com>
2018-05-18 15:55:59 +0200

    add offer service and repo

---
+++ src/main/java/hr/tvz/java/teambuildingbooking/service/impl/OfferServiceImpl.java
@@ -0,0 +1,27 @@
+package hr.tvz.java.teambuildingbooking.service.impl;
+
+import hr.tvz.java.teambuildingbooking.model.Offer;
+import hr.tvz.java.teambuildingbooking.repository.OfferRepository;
+import hr.tvz.java.teambuildingbooking.service.OfferService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class OfferServiceImpl implements OfferService {
+
+    private OfferRepository offerRepository;
+
+    @Autowired
+    public OfferServiceImpl(OfferRepository oR) {
+        this.offerRepository = oR;
+    }
+
+    @Override
+    public List<Offer> findAll() {
+        return offerRepository.findAll();
+    }
+}
