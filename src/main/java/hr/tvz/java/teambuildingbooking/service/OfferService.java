4d20297453007731f397644b8a830cc06777ddb7
Filip Blažević <fblazevic@tvz.hr>
2018-05-25 20:45:43 +0200

    Update Backend for new-offer
    Update OfferService

--- src/main/java/hr/tvz/java/teambuildingbooking/service/OfferService.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/service/OfferService.java
@@ -1,7 +1,9 @@
 package hr.tvz.java.teambuildingbooking.service;
 
 import hr.tvz.java.teambuildingbooking.model.Offer;
+import hr.tvz.java.teambuildingbooking.model.form.NewOfferForm;
 
+import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,6 @@ public interface OfferService {
     List<Offer> findTopOffers();
 
    Optional<Offer> findOne(Long id);
+
+   Offer createOffer(NewOfferForm newOfferForm) throws ParseException;
 }
