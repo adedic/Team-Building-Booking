ff32549f010effa6c47cc2433a6be20674e512b4
Ana Dedić <anaaadedic@gmail.com>
2018-05-25 14:16:10 +0200

    add datepicker on search offer and details

--- src/main/webapp/resources/static/js/custom.js
+++ src/main/webapp/resources/static/js/custom.js
@@ -1,5 +1,31 @@
 $(function() {
-    $('input[name="datePicker"]').daterangepicker({
+    $('input[name="date"]').daterangepicker({
+        opens: 'down',
+        singleDatePicker: true,
+        locale: {
+            cancelLabel: 'Odustani',
+            format: 'DD.MM.YYYY.'
+        }
+    }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
+    });
+});
+
+$(function() {
+    $('input[name="dateFrom"]').daterangepicker({
+        opens: 'down',
+        singleDatePicker: true,
+        locale: {
+            cancelLabel: 'Odustani',
+            format: 'DD.MM.YYYY.'
+        }
+    }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
+    });
+});
+
+$(function() {
+    $('input[name="dateTo"]').daterangepicker({
         opens: 'down',
         singleDatePicker: true,
         locale: {
