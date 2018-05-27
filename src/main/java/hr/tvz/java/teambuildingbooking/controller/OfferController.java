a4ce7f3499f4048bde6dd12f5f45e1b255b4312b
mpuljic <maja.ljicpu@gmail.com>
2018-05-27 22:12:48 +0200

    Ispravak pretrage ponuda
    Ispravljena logika oko broja osoba

--- src/main/java/hr/tvz/java/teambuildingbooking/controller/OfferController.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/controller/OfferController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Optional;
