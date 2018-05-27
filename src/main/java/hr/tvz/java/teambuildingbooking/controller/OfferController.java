c8761a054bfa160d618be465c74893c8494fbbe5
Marko Štuban <marko.stuban@ag04.com>
2018-05-27 18:38:17 +0200

    Added search parameter badges on search results view, mapped searchOfferForm to thymeleaf

--- src/main/java/hr/tvz/java/teambuildingbooking/controller/OfferController.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/controller/OfferController.java
@@ -8,17 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 
 @Controller
 @RequestMapping("/offer")
+@SessionAttributes("searchOfferForm")
 public class OfferController {
 
     private static final String NEW_OFFER_VIEW_NAME = "offer/new-offer";
@@ -43,12 +43,13 @@ public class OfferController {
     private String searchOffer(Model model) {
         model.addAttribute("categories", categoryService.findAll());
         model.addAttribute("topOffers", offerService.findTopOffers());
+        model.addAttribute("searchOfferForm", new SearchOfferForm());
         return SEARCH_OFFER_VIEW_NAME;
     }
 
     @PostMapping("/search")
-    private String findSearchResults(Model model, SearchOfferForm searchOffer) {
-        model.addAttribute("offers", offerService.findOffers(searchOffer));
+    private String findSearchResults(@Valid @ModelAttribute("searchOfferForm") SearchOfferForm searchOfferForm, Model model) {
+        model.addAttribute("offers", offerService.findOffers(searchOfferForm));
         return SEARCH_RESULTS_VIEW_NAME;
     }
 
@@ -62,7 +63,7 @@ public class OfferController {
     @RequestMapping("/details/{id}")
     private String showDetails(Model model, @PathVariable("id") Long id) {
         Optional<Offer> offer = offerService.findOne(id);
-        if(offer.isPresent()){
+        if (offer.isPresent()) {
             model.addAttribute("offer", offer.get());
         }
 
@@ -72,7 +73,7 @@ public class OfferController {
     @RequestMapping("/details/{id}/reviews")
     private ModelAndView showReviews(Model model, @PathVariable("id") Long id) {
         Optional<Offer> offer = offerService.findOne(id);
-        if(offer.isPresent()){
+        if (offer.isPresent()) {
             model.addAttribute("feedbacks", offer.get().getFeedbacks());
         }
 
