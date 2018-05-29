4c7ec075481ce240fd0211e33dbb787b245ee8d6
mpuljic <maja.ljicpu@gmail.com>
2018-05-29 22:26:55 +0200

    Ispravak pretrage ponuda

--- src/main/java/hr/tvz/java/teambuildingbooking/repository/OfferDaoRepositoryImpl.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/repository/OfferDaoRepositoryImpl.java
@@ -1,15 +1,13 @@
 package hr.tvz.java.teambuildingbooking.repository;
 
+import hr.tvz.java.teambuildingbooking.model.Category;
 import hr.tvz.java.teambuildingbooking.model.Offer;
 import hr.tvz.java.teambuildingbooking.model.criteria.SearchCriteria;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,32 +22,34 @@ public class OfferDaoRepositoryImpl implements OfferDaoRepository {
     public List<Offer> findOffers(List<SearchCriteria> searchCriteria) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Offer> query = builder.createQuery(Offer.class);
-        Root r = query.from(Offer.class);
+        Root<Offer> rootOffer = query.from(Offer.class);
 
         Predicate predicate = builder.conjunction();
 
         for (SearchCriteria criteria : searchCriteria) {
             if (criteria.getOperation().equalsIgnoreCase(">")) {
-                predicate = builder.and(predicate, builder.greaterThanOrEqualTo(r.get(criteria.getKey()), criteria.getValue().toString()));
+                predicate = builder.and(predicate, builder.greaterThanOrEqualTo(rootOffer.get(criteria.getKey()), criteria.getValue().toString()));
             } else if (criteria.getOperation().equalsIgnoreCase("<")) {
-                predicate = builder.and(predicate, builder.lessThanOrEqualTo(r.get(criteria.getKey()), criteria.getValue().toString()));
+                predicate = builder.and(predicate, builder.lessThanOrEqualTo(rootOffer.get(criteria.getKey()), criteria.getValue().toString()));
             } else if (criteria.getOperation().equalsIgnoreCase(":")) {
-                if (r.get(criteria.getKey()).getJavaType() == String.class) {
-                    predicate = builder.and(predicate, builder.like(r.get(criteria.getKey()), "%" + criteria.getValue() + "%"));
+                if (rootOffer.get(criteria.getKey()).getJavaType() == String.class) {
+                    predicate = builder.and(predicate, builder.like(rootOffer.get(criteria.getKey()), "%" + criteria.getValue() + "%"));
                 } else {
-                    predicate = builder.and(predicate, builder.equal(r.get(criteria.getKey()), criteria.getValue()));
+                    predicate = builder.and(predicate, builder.equal(rootOffer.get(criteria.getKey()), criteria.getValue()));
                 }
             } else if (criteria.getOperation().equalsIgnoreCase(":>")) {
-                predicate = builder.and(predicate, builder.lessThanOrEqualTo(r.get(criteria.getKey()), (Date) criteria.getValue()));
+                predicate = builder.and(predicate, builder.lessThanOrEqualTo(rootOffer.get(criteria.getKey()), (Date) criteria.getValue()));
             } else if (criteria.getOperation().equalsIgnoreCase(":<")) {
-                predicate = builder.and(predicate, builder.greaterThanOrEqualTo(r.get(criteria.getKey()), (Date) criteria.getValue()));
+                predicate = builder.and(predicate, builder.greaterThanOrEqualTo(rootOffer.get(criteria.getKey()), (Date) criteria.getValue()));
+            } else if (criteria.getOperation().equalsIgnoreCase("//")) {
+                predicate = builder.and(predicate, builder.equal(rootOffer.join("categories").get("id"), criteria.getValue()));
             }
 
         }
 
-        predicate = builder.and(predicate, builder.isNull(r.get("dateDeleted")));
-        predicate = builder.and(predicate, builder.isTrue(r.get("active")));
-        predicate = builder.and(predicate, builder.isTrue(r.get("enabled")));
+        predicate = builder.and(predicate, builder.isNull(rootOffer.get("dateDeleted")));
+        predicate = builder.and(predicate, builder.isTrue(rootOffer.get("active")));
+        predicate = builder.and(predicate, builder.isTrue(rootOffer.get("enabled")));
 
         query.where(predicate);
 
--- src/main/java/hr/tvz/java/teambuildingbooking/service/impl/OfferServiceImpl.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/service/impl/OfferServiceImpl.java
@@ -46,7 +46,7 @@ public class OfferServiceImpl implements OfferService {
         List<SearchCriteria> searchCriteria = new ArrayList<>();
         if(searchOffer != null) {
             if(searchOffer.getCategory() != null) {
-                searchCriteria.add(new SearchCriteria("categories", ":", searchOffer.getCategory()));
+                searchCriteria.add(new SearchCriteria("categoryId", "//", searchOffer.getCategory()));
             }
             if(searchOffer.getCity() != null && !searchOffer.getCity().equals("")) {
                 searchCriteria.add(new SearchCriteria("city", ":", searchOffer.getCity()));
