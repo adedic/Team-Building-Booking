f4611865f5e8316c448c0d665a2eed202e267e39
Filip Blažević <fblazevic@tvz.hr>
2018-06-01 09:30:16 +0200

    Add about and contact screens

--- src/main/java/hr/tvz/java/teambuildingbooking/controller/IndexController.java
+++ src/main/java/hr/tvz/java/teambuildingbooking/controller/IndexController.java
@@ -12,9 +12,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     private static final String OFFER_SEARCH_REDIRECT = "redirect:/offer/search";
+    private static final String ABOUT_VIEW_NAME = "about";
+    private static final String CONTACT_VIEW_NAME = "contact";
 
     @GetMapping({"/", "index"})
     public String index(Model model) {
         return OFFER_SEARCH_REDIRECT;
+    }
+
+    @GetMapping({"/about"})
+    public String about() {
+        return ABOUT_VIEW_NAME;
+    }
+
+    @GetMapping({"/contact"})
+    public String contact(){
+        return CONTACT_VIEW_NAME;
     }
 }
